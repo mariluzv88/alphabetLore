c1112541233c071548d248d4afd2f90fdd31a57b
Mariluz Vargas <mariluzv88@gmail.com>
2023-06-04 11:52:51 -0400

    fisrt commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let AAH = <img> src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7b3ba388-3ba0-4f0a-af76-2d87184cb3d2/dffom42-eca8e588-00f8-41bb-846f-00455e4bf324.png/v1/fill/w_799,h_999/a_jumps_in_fear__alphabet_lore__by_blushneki522_dffom42-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTEwMCIsInBhdGgiOiJcL2ZcLzdiM2JhMzg4LTNiYTAtNGYwYS1hZjc2LTJkODcxODRjYjNkMlwvZGZmb200Mi1lY2E4ZTU4OC0wMGY4LTQxYmItODQ2Zi0wMDQ1NWU0YmYzMjQucG5nIiwid2lkdGgiOiI8PTg4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.NM0W1UUFpHBCzhMmcPOh_VK5aOT8j7iM1kmhXtgKFb4" alt="" class="let"</img>
+
+
+
+let loreA = document.querySelector(".letterA")
+
+// const replaceLetter = (lore) => {
+//     span.innerHTML(lore)
+// }
+
+const enter = (lore) => {
+    if(lore === 'AAH'){
+        letterA.innerHTML = "" 
+       letterA.append (AAH)
+    }
+}
